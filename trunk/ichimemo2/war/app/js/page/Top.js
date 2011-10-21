be0b5d852509c69ad29da7ced6b4045b6aa040ad
kotemaru@kotemaru.org <kotemaru@kotemaru.org@a347e8cc-9a8b-d672-4536-a9ce3f1b6410>
2011-10-21 00:57:47 +0000

    Persistent jsessionid cookie support.

--- trunk/ichimemo2/war/app/js/page/Top.js
+++ trunk/ichimemo2/war/app/js/page/Top.js
@@ -1,13 +1,30 @@
 var Top = Page.def(function Top(){}, function(Class){
 
+Class.waitCount = 0;
+	
 Class.onBeforeShow = function() {
+	setTimeout(Class.loginCheck, 100);
+}
+
+Class.loginCheck = function() {
+	if (PCookie.done || Class.waitCount>10) {
+		Login.user = Kokorahen.getLoginUser();
+	} else {
+		Class.waitCount++;
+		console.log("Wait PCookie.load ", Class.waitCount);
+		setTimeout(Class.loginCheck, 100);
+		return;
+	}
 	if (Login.user == null) {
 		Class.changePage("#Login");
 	} else {
 		Class.changePage(Map.ID);
-		//Map.setCenterFromGPS();
 	}
 }
+
+
+
+
 Class.changePage = function(id) {
 	$.mobile.showPageLoadingMsg();
 	setTimeout(function(){
