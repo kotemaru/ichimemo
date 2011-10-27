3e22cc1846e56f209bebece41d87088ba6842b85
kotemaru@kotemaru.org <kotemaru@kotemaru.org@a347e8cc-9a8b-d672-4536-a9ce3f1b6410>
2011-10-27 07:52:59 +0000

    SpotTags refact

--- ichimemo2/war/app/js/page/SpotTags.js
+++ ichimemo2/war/app/js/page/SpotTags.js
@@ -1,118 +1,138 @@
 
-var SpotTags = Page.def(function SpotTags(){}, function(Class){
-
-Class.SELECTOR = "#spotTagsList";
-Class.button = null;
-
-	var TAG_TREE = 
-	[
-		"食事",[
-		      "和食",
-		      "洋食",
-		      "中華料理",
-		      "イタリアン",
-		      "フレンチ",
-		      "エスニック",
-		      "ラーメン",
-		      "カレー",
-		],
-		"呑む",[
-			      "日本酒",
-			      "焼酎",
-			      "ワイン",
-			      "ビール",
+Page.def(function SpotTags(){}, function(Class){
+	
+	var TAG_TREE = [
+			"食事",[
+			      "和食",
+			      "洋食",
+			      "中華料理",
+			      "イタリアン",
+			      "フレンチ",
+			      "エスニック",
+			      "ラーメン",
+			      "カレー",
 			],
-		"喫茶",[
-		      "コーヒー",
-		      "紅茶",
-		      "日本茶",
-		      "スイーツ",
-		      "軽食",
-		],
-		"買物",[
-		      "百貨店",
-		      "スーパー",
-		      "コンビニ",
-		      "ドラッグショップ",
-		      "100円ショップ",
-		      "衣料品",
-		      "生活雑貨",
-		      "酒屋"
-		],
-		"行楽",[
-		      "公園",
-		      "イベント",
-		      "カラオケ",
-		      "映画",
-		],
-		"病院",[
-		      "内科",
-		      "外科",
-		      "小児科",
-		      "産婦人科",
-		      "耳鼻科",
-		      "眼科",
-		      "皮膚科",
-		],
+			"呑む",[
+				      "日本酒",
+				      "焼酎",
+				      "ワイン",
+				      "ビール",
+				],
+			"喫茶",[
+			      "コーヒー",
+			      "紅茶",
+			      "日本茶",
+			      "スイーツ",
+			      "軽食",
+			],
+			"買物",[
+			      "百貨店",
+			      "スーパー",
+			      "コンビニ",
+			      "ドラッグショップ",
+			      "100円ショップ",
+			      "衣料品",
+			      "生活雑貨",
+			      "酒屋"
+			],
+			"行楽",[
+			      "公園",
+			      "イベント",
+			      "カラオケ",
+			      "映画",
+			],
+			"病院",[
+			      "内科",
+			      "外科",
+			      "小児科",
+			      "産婦人科",
+			      "耳鼻科",
+			      "眼科",
+			      "皮膚科",
+			],
 	];
-Class.searchTags = [];
-Class.formTags = [];
-Class.openMode = null;
+	Class.MYSPOT = "myspot";
+	Class.SEARCH = "search";
+	Class.SPOT = "spot";
 
+	var button = null;
+	var openMode = null;
+	var placeMsg = null;
+	var selectorMy = null;
+	var selector = null;
+	var values = {
+		search: [],
+		spot: [],
+		myspot: []
+	};
 
-Class.onBeforeShow = function() {
-	Class.selector.refresh();
-}
-Class.open = function(btn, placeMsg, mode) {
-	if (Class.selector == null) {
-		if (Login.user.tags != null) {
-			tagTree = TAG_TREE.concat(Login.user.tags);
-		}
-		Class.selector = new Selector(Class.SELECTOR, tagTree);
+	
+	Class.onBeforeShow = function() {
+		var sel = getSelector(openMode);
+		sel.refresh();
+	}
+	Class.open = function(btn, placeMsg, mode) {
+		button = btn;
+		placeMsg = placeMsg;
+		openMode = mode;
+		var sel = getSelector(openMode);
+		sel.isSingle = (openMode == Class.SEARCH);
+		sel.setValue(values[mode]);
+		Util.dialog(Class.ID);
+	}
+	
+	Class.onHide = function() {
+		var sel = getSelector(openMode);
+		values[openMode] = sel.getValue();
+		setLabel(button, values[openMode], placeMsg);
+	}
+	
+	Class.getSearchTag = function() {
+		if (values.search.length == 0) return null;
+		return values.search[0];
+	}
+	
+	Class.setValue = function(mode, list) {
+		values[mode] = list;
+	}
+	Class.getValue = function(mode) {
+		return values[mode];
 	}
 
-	Class.button = btn;
-	Class.placeMsg = placeMsg;
-	Class.openMode = mode;
-	if (mode == "search") {
-		Class.selector.setValue(Class.searchTags);
-		Class.selector.isSingle = true;
-	} else {
-		Class.selector.setValue(Class.formTags);
-		Class.selector.isSingle = false;
-	}
-
-	Util.dialog(Class.ID);
-}
-
-Class.onHide = function() {
-	var list = Class.selector.getValue();
-	if (Class.openMode == "search") {
-		Class.searchTags = list;
-	} else {
-		Class.formTags = list;
-	}
-	Class.setLabel(Class.button,list,Class.placeMsg);
-}
-Class.setFormTags = function(list) {
-	Class.formTags = list;
-}
-Class.setLabel = function(btn, list, placeMsg) {
-	var label = list.join(",");
-	if (label == "") label = placeMsg;
-	$(btn).each(function(){
-		var span = $(this).find(".ui-btn-text");
-		if (span.length > 0) {
-			span.text(label);
+	function getSelector(mode) {
+		var isMy = (mode == Class.SEARCH || mode == Class.MYSPOT);
+		if (isMy) {
+			if (selectorMy == null) {
+				var tagTree = TAG_TREE;
+				if (Login.user.tags != null) {
+					tagTree = tagTree.concat(Login.user.tags);
+				}
+				selectorMy = new Selector($(Class.PAGE).find(".TagListMy")[0], tagTree);
+			}
+			$(Class.PAGE).find(".TagListMy").show();
+			$(Class.PAGE).find(".TagList").hide();
+			return selectorMy;
 		} else {
-			$(this).text(label);
+			if (selector == null) {
+				selector = new Selector($(Class.PAGE).find(".TagList")[0], TAG_TREE);
+			}
+			$(Class.PAGE).find(".TagList").show();
+			$(Class.PAGE).find(".TagListMy").hide();
+			return selector;
 		}
-	});
-}
-
-Class.getSearchTag = function() {
-	if (Class.searchTags.length == 0) return null;
-	return Class.searchTags[0];
-}
+	}
+	function setLabel(btn, list, placeMsg) {
+		var label = list.join(",");
+		if (label == "") label = placeMsg;
+		$(btn).each(function(){
+			var span = $(this).find(".ui-btn-text");
+			if (span.length > 0) {
+				span.text(label);
+			} else {
+				$(this).text(label);
+			}
+		});
+	}
+	Class.setLabel = setLabel;
 
 });
